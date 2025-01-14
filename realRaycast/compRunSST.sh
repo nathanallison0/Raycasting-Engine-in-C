a71239b86e9bfe470aae6376308d05385783e3fa
Nathan Allison <nathanallison26@gmail.com>
2025-01-14 17:30:12 -0500

    Removed .sh files from repository.

---
+++ realRaycast/compRunSST.sh
@@ -0,0 +1,6 @@
+fName=SaveSTates
+directory="/Users/nallison/Library/CloudStorage/OneDrive-MinutemanRegionalVocationalTechnicalSchoolDistrict/ProgrammingInClassPC/myC/SDL/realRaycast"
+cd $directory
+clear
+gcc $fName.c -o $fName -Wall || exit 1
+$directory/$fName
