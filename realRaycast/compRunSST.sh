8bfe794b9f77c06d2ced4e79b3f2a095cf92eb25
Nathan Allison <nathanallison26@gmail.com>
2025-01-14 17:31:00 -0500

    Removed unwanted .sh files.

--- realRaycast/compRunSST.sh
+++
@@ -1,6 +0,0 @@
-fName=SaveSTates
-directory="/Users/nallison/Library/CloudStorage/OneDrive-MinutemanRegionalVocationalTechnicalSchoolDistrict/ProgrammingInClassPC/myC/SDL/realRaycast"
-cd $directory
-clear
-gcc $fName.c -o $fName -Wall || exit 1
-$directory/$fName
