12c4908f863dfdf4c455e3f5eabedb053f32211a
Nathan Allison <nathanallison26@gmail.com>
2025-01-14 17:28:49 -0500

    Removed .sh files from repository.

--- DebugTerminal/compRun.sh
+++
@@ -1,5 +0,0 @@
-fName=DebugTerminal
-directory="/Users/nallison/Library/CloudStorage/OneDrive-MinutemanRegionalVocationalTechnicalSchoolDistrict/ProgrammingInClassPC/myC/$fName"
-cd $directory
-gcc $fName.c -o $fName -Wall || exit 1
-$directory/$fName
